dc223d5eee66820934cb26610b6f5215d034e1fc
PedroByJoao <joaoexplorer235@gmail.com>
2025-12-24 18:25:27 -0300

    tests: verify order of applicative and normal order red

--- test/FullBetaRSpec.hs
+++ test/FullBetaRSpec.hs
@@ -115,3 +115,54 @@ spec = do
       testFullBetaReductions "Application of variables stays unchanged"
         (App (Var x) (Var y))
         (App (Var x) (Var y))
+
+  describe "Applicative: Single-step reduction order" $ do
+
+    describe "Applicative Order (rightmost innermost first)" $ do
+      -- (λx.x b) ((λy.y) a)
+      -- Applicative should reduce the argument first: ((λy.y) a) -> a
+      -- Result after 1 step: (λx.x b) a
+      it "reduces argument before function application" $ do
+        let expr = App (Lam x (App (Var x) (Var b))) (App (Lam y (Var y)) (Var a))
+        applicative expr `shouldBe` Just (App (Lam x (App (Var x) (Var b))) (Var a))
+
+      -- (λx.(λy.y) x) a
+      -- Applicative should reduce inside the lambda body first: (λy.y) x -> x
+      -- Result after 1 step: (λx.x) a
+      it "reduces inside lambda body before outer redex" $ do
+        let expr = App (Lam x (App (Lam y (Var y)) (Var x))) (Var a)
+        applicative expr `shouldBe` Just (App (Lam x (Var x)) (Var a))
+
+      -- ((λx.x) a) ((λy.y) b)
+      -- Applicative: rightmost innermost first, so reduce ((λy.y) b) -> b
+      it "reduces rightmost redex first in application" $ do
+        let expr = App (App (Lam x (Var x)) (Var a)) (App (Lam y (Var y)) (Var b))
+        applicative expr `shouldBe` Just (App (App (Lam x (Var x)) (Var a)) (Var b))
+
+    describe "Normal Order (leftmost outermost first)" $ do
+      -- (λx.x b) ((λy.y) a)
+      -- Normal order should reduce the outermost redex first
+      -- Result after 1 step: ((λy.y) a) b
+      it "reduces outermost redex before argument" $ do
+        let chosenRedex = (App (Lam y (Var y)) (Var a))
+            expr = App (Lam x (App (Var x) (Var b))) chosenRedex
+        normalOrder expr `shouldBe` Just (App chosenRedex (Var b))
+
+      -- (λx.(λy.y) x) a
+      -- Normal order should reduce the outer application first
+      -- Result after 1 step: (λy.y) a
+      it "reduces outer redex before inner redex in body" $ do
+        let expr = App (Lam x (App (Lam y (Var y)) (Var x))) (Var a)
+        normalOrder expr `shouldBe` Just (App (Lam y (Var y)) (Var a))
+
+      -- λx.(λy.y) x
+      -- Normal order still reduces inside lambdas (unlike call-by-name)
+      it "reduces inside lambda abstraction" $ do
+        let expr = Lam x (App (Lam y (Var y)) (Var x))
+        normalOrder expr `shouldBe` Just (Lam x (Var x))
+
+      -- ((λx.x) a) ((λy.y) b)
+      -- Normal order: leftmost outermost first, so reduce ((λx.x) a) -> a
+      it "reduces leftmost redex first in application" $ do
+        let expr = App (App (Lam x (Var x)) (Var a)) (App (Lam y (Var y)) (Var b))
+        normalOrder expr `shouldBe` Just (App (Var a) (App (Lam y (Var y)) (Var b)))
